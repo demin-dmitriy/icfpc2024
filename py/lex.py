c2b3b1d205620efe416e287fac0a9ef284b5f476
Demin Dmitriy <demindf@gmail.com>
2024-06-28 16:32:05 +0300

    add lex.py

---
+++ py/lex.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import sys
+from argparse import ArgumentParser
+from enum import Enum
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`|~ \n"
+
+
+class Op(Enum):
+    pass
+
+class Ternary(Op):
+    If = 1
+
+    def __repr__(self):
+        return self.name
+
+
+class Binary(Op):
+    Add = '+'   # Integer addition    B+ I# I$ -> 5
+    Sub = '-'   # Integer subtraction    B- I$ I# -> 1
+    Mul = '*'   # Integer multiplication    B* I$ I# -> 6
+    Div = '/'   # Integer division (truncated towards zero)    B/ U- I( I# -> -3
+    Mod = '%'   # Integer modulo    B% U- I( I# -> -1
+    Less = '<'   # Integer comparison    B< I$ I# -> false
+    Greater = '>'   # Integer comparison    B> I$ I# -> true
+    Eq = '='   # Equality comparison, works for int, bool and string    B= I$ I# -> false
+    Or = '|'   # Boolean or    B| T F -> true
+    And = '&'   # Boolean and    B& T F -> false
+    Concat = '.'   # String concatenation    B. S4% S34 -> "test"
+    Take = 'T'   # Take first x chars of string y    BT I$ S4%34 -> "tes"
+    Drop = 'D'   # Drop first x chars of string y    BD I$ S4%34 -> "t"
+    Apply = '$'   # Apply term x to y (see Lambda abstractions)
+
+    def __repr__(self):
+        return self.value
+
+
+class Unary(Op):
+    Neg = '-' #    Integer negation    U- I$ -> -3
+    Not = '!' #    Boolean not    U! T -> false
+    ToInt = '#' #    string-to-int: interpret a string as a base-94 number    U# S4%34 -> 15818151
+    ToStr = '$' #    int-to-string: inverse of the above    U$ I4%34 -> test
+
+    def __repr__(self):
+
+        return self.value
+
+
+
+def parse_source_char(ch: str) -> int:
+    return ord(ch) - 33
+
+
+def parse_int(s: str):
+    return sum(94**i * parse_source_char(ch) for i, ch in enumerate(s[::-1]))
+
+
+def parse_string(s: str):
+    return ''.join(
+        alphabet[parse_source_char(i)]
+        for i in s
+    )
+
+
+def parse_token(tok):
+    match tok[0]:
+        case 'S':
+            return parse_string(tok[1:])
+        case '?':
+            return Ternary.If
+        case 'T':
+            return True
+        case 'F':
+            return False
+        case 'I':
+            return parse_int(tok[1:])
+        case 'B':
+            return Binary(tok[1:])
+        case 'U':
+            return Unary(tok[1:])
+
+    return tok
+
+
+
+def parse_prog(icfp):
+    tokens = [parse_token(tok) for tok in icfp.split(' ')]
+    return tokens
+
+
+def main():
+    parser = ArgumentParser('lex')
+    parser.add_argument('icfp')
+    args = parser.parse_args()
+    icfp = args.icfp
+    print(parse_prog(icfp))
+
+
+if __name__ == '__main__':
+    main()
+
