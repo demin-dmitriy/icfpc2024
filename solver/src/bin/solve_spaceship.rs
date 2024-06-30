583f9827aa28ad6b2343b69e61a1a869c96fd470
Nikita Ivanov <ramzesxi@gmail.com>
2024-06-30 20:31:56 +0300

    update main

--- solver/src/bin/solve_spaceship.rs
+++ solver/src/bin/solve_spaceship.rs
@@ -7,61 +7,64 @@ fn run_solvers(
     problem_filepath: String,
     solution_filepath: String
 ) {
-    let positions = read_positions(&problem_filepath);
-    let best_solution = read_from_file(&solution_filepath);
-    
-    let mut solvers: Vec<(&str, fn(Vec<Pos>) -> Option<Vec<Step>>)> = Vec::new();
-    //solvers.push(("Greedy max speed", solve_greedy));
-    //solvers.push(("Greedy move towards", solve_greedy_towards));
-    //solvers.push(("Greedy try min, fallback max speed", solve_greedy_try_min));
-    //solvers.push(("Greedy try min, fallback towards", solve_greedy_try_min_and_towards));
-    //solvers.push(("Greedy use quadtree, try min, fallback towards", solve_greedy_quadtree_try_min_and_towards));
-    //solvers.push(("Greedy use quadtree, exact min, fallback towards", solve_greedy_quadtree_exact_min_and_towards));
-    //solvers.push(("Greedy min step, exact min", solve_greedy_min_step_exact_move));
-    //solvers.push(("Greedy min step iterative, exact min", solve_greedy_min_step_iterative_exact_move));
-    solvers.push(("Greedy min step random, exact min", solve_greedy_min_step_random_exact_move));
+    loop {
+        let positions = read_positions(&problem_filepath);
+        let best_solution = read_from_file(&solution_filepath);
+        
+        let mut solvers: Vec<(&str, fn(Vec<Pos>) -> Option<Vec<Step>>)> = Vec::new();
+        //solvers.push(("Greedy max speed", solve_greedy));
+        //solvers.push(("Greedy move towards", solve_greedy_towards));
+        //solvers.push(("Greedy try min, fallback max speed", solve_greedy_try_min));
+        //solvers.push(("Greedy try min, fallback towards", solve_greedy_try_min_and_towards));
+        //solvers.push(("Greedy use quadtree, try min, fallback towards", solve_greedy_quadtree_try_min_and_towards));
+        //solvers.push(("Greedy use quadtree, exact min, fallback towards", solve_greedy_quadtree_exact_min_and_towards));
+        //solvers.push(("Greedy min step, exact min", solve_greedy_min_step_exact_move));
+        //solvers.push(("Greedy min step iterative, exact min", solve_greedy_min_step_iterative_exact_move));
+        solvers.push(("Greedy min step random, exact min", solve_greedy_min_step_random_exact_move));
 
-    let mut min_score = usize::MAX;
-    let mut new_best_solution: Option<Vec<Step>> = None;
-    for (solver_name, solver) in solvers.iter() {
-        println!("Task '{}', run solver '{}'", problem_filepath, solver_name);
-        let solution = solver(positions.clone());
-        match solution {
-            Some(moves) => {
-                println!("Task '{}', score: {} (solver '{}')", problem_filepath, moves.len(), solver_name);
-                if moves.len() < min_score {
-                    new_best_solution = Some(moves.clone());
-                    min_score = moves.len();
-                }
-            },
-            None => {
-                println!("Task '{}', solution not found (solver '{}')", problem_filepath, solver_name);
-            }
-        }
-    }
-
-    match new_best_solution {
-        Some(new_best) => {
-            let mut need_save = false;
-            match best_solution {
-                Some(old_best) => {
-                    if new_best.len() < old_best.len() {
-                        println!("Task '{}', new best score: {}! (old best: {})", problem_filepath, new_best.len(), old_best.len());
-                        need_save = true;
+        let mut min_score = usize::MAX;
+        let mut new_best_solution: Option<Vec<Step>> = None;
+        for (solver_name, solver) in solvers.iter() {
+            println!("Task '{}', run solver '{}'", problem_filepath, solver_name);
+            let solution = solver(positions.clone());
+            match solution {
+                Some(moves) => {
+                    println!("Task '{}', score: {} (solver '{}')", problem_filepath, moves.len(), solver_name);
+                    if moves.len() < min_score {
+                        new_best_solution = Some(moves.clone());
+                        min_score = moves.len();
                     }
                 },
                 None => {
-                    println!("Task '{}', new best score {}!", problem_filepath, new_best.len());
-                    need_save = true;
+                    println!("Task '{}', solution not found (solver '{}')", problem_filepath, solver_name);
                 }
             }
-            if need_save {
-                write_to_file(&solution_filepath, &new_best);
-            }
-        },
-        None => {
-            println!("Task '{}', solution not found", problem_filepath);
         }
+
+        match new_best_solution {
+            Some(new_best) => {
+                let mut need_save = false;
+                match best_solution {
+                    Some(old_best) => {
+                        if new_best.len() < old_best.len() {
+                            println!("Task '{}', new best score: {}! (old best: {})", problem_filepath, new_best.len(), old_best.len());
+                            need_save = true;
+                        }
+                    },
+                    None => {
+                        println!("Task '{}', new best score {}!", problem_filepath, new_best.len());
+                        need_save = true;
+                    }
+                }
+                if need_save {
+                    write_to_file(&solution_filepath, &new_best);
+                }
+            },
+            None => {
+                println!("Task '{}', solution not found", problem_filepath);
+            }
+        }
+        break;
     }
 }
 
